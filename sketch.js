db5770e97b7e0507642c7827f056cfee981abfd7
Michelle Wu <michellewu@10-18-163-87.dynapool.wireless.nyu.edu>
2022-12-03 18:14:03 -0500

    figured out sound functionality

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let beef, croutons, cucumber, cranberry, ham, lettuce, pasta, pepper, pineapple, quinoa, tomato;
 let beefImg, croutonsImg;
 let beefIcon, croutonsIcon;
+let beefPlaying, croutonsPlaying = false;
 
 function preload() {
   beef = loadSound('sounds/beef.edit.wav');
@@ -29,6 +30,10 @@ function setup() {
 function draw() {
   background(160, 136, 111);
 
+  stroke(0);
+  strokeWeight(4);
+  ellipse(width/2, height/2, 200);
+
   beefIcon.over();
   beefIcon.update();
   let beefImgX = constrain(beefIcon.x, 0, 600 - beefIcon.w);
@@ -42,6 +47,35 @@ function draw() {
   let croutonsImgY = constrain(croutonsIcon.y, 0, 600 - croutonsIcon.h);
   image(croutonsImg, croutonsImgX, croutonsImgY, croutonsIcon.w, croutonsIcon.h);
   croutonsIcon.show();
+
+  if (isInsideBowl(beefImgX, beefImgY)) {
+    if (!beefPlaying) {
+      beefPlaying = true;
+      beef.loop();
+    }
+  } else {
+    beefPlaying = false;   
+    beef.stop();
+  }
+
+  if (isInsideBowl(croutonsImgX, croutonsImgY)) {
+    if (!croutonsPlaying) {
+      croutonsPlaying = true;
+      croutons.loop();
+    }
+  } else {
+    croutonsPlaying = false;   
+    croutons.stop();
+  }
+
+
+}
+
+function isInsideBowl(x, y) {
+  if (x >= 200 && x <= 400 && y >= 200 && y <= 400) {
+    return true;
+  }
+  return false;
 }
 
 function mousePressed() {
